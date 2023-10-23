7ac1e89bd30c3b8c75575514100115e5ccb852f2
drkying <yatsunyin@gmail.com>
2023-10-23 17:16:07 +0800

    增加firebase && github action

--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+import {getAnalytics} from "firebase/analytics";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD8dzO9a4f1aP7bBIdnMAHdNciU-b_1TQI",
+    authDomain: "checkerphone-e4c2e.firebaseapp.com",
+    projectId: "checkerphone-e4c2e",
+    storageBucket: "checkerphone-e4c2e.appspot.com",
+    messagingSenderId: "891601599657",
+    appId: "1:891601599657:web:a0ed5fcda807c71d0d7db8",
+    measurementId: "G-5XHS3ERFW5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
